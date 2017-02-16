a62fcbeb6fc40974432010cb9f4104815987073d
jasper <jasper.ji@comactivity.com.au>
2017-02-16 15:26:11 +1100

    Add MockMVC end to end test

---
+++ Spring_MVC/src/main/test/spring/mvc/mockmvctest/MockMVCTest.java
@@ -0,0 +1,46 @@
+package main.test.spring.mvc.mockmvctest;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "file:WebContent/WEB-INF/Conf/Spring.xml" })
+public class MockMVCTest {
+
+	@Autowired
+	WebApplicationContext wac;
+	@Autowired
+	MockHttpSession session;
+	@Autowired
+	MockHttpServletRequest request;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void getAccount() throws Exception {
+
+		this.mockMvc.perform(get("/Rest/Test").accept(MediaType.TEXT_HTML))
+				.andExpect(status().isOk()).andExpect(content().string(containsString("Test")));
+	}
+}
