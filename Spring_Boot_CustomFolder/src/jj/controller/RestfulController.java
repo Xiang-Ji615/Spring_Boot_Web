183dbebade94892a9dfb647aa14dbe5fe978d203
xiang <xiang@Jasper-Surface>
2017-07-17 10:30:26 +1000

    Add @InitBinder and @ModelAttribute support

--- Spring_Boot_CustomFolder/src/jj/controller/RestfulController.java
+++ Spring_Boot_CustomFolder/src/jj/controller/RestfulController.java
@@ -1,15 +1,47 @@
 package jj.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jj.model.User;
 
 @RestController
 @RequestMapping(value="Rest")
 public class RestfulController {
 
 	@RequestMapping(value="HelloWorld", method=RequestMethod.GET)
-	public Object helloWorld(){
+	public Object helloWorld(@RequestParam("dob") Date date){
+		System.out.println(date);
 		return "Hello world";
+	}
+	
+	@RequestMapping(value="ModelAttr", method=RequestMethod.GET)
+	public Object modelAttribute(@ModelAttribute User user, BindingResult bindingResult) {
+		System.out.println(user.getDob());
+		if(bindingResult.hasErrors()) {
+			System.out.println(bindingResult.getErrorCount());
+		}
+		return user;
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+		dateFormat.setLenient(false);
+		binder.registerCustomEditor(Date.class, "dob", new CustomDateEditor(dateFormat, true));
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+//		binder.addCustomFormatter(new DateFormatter("dd/MM/yyyy"));
+//		System.out.println("Init Binder runs");
 	}
 }
---
+++ Spring_Boot_CustomFolder/src/jj/model/User.java
@@ -0,0 +1,35 @@
+package jj.model;
+
+import java.util.Date;
+
+public class User {
+
+	String username;
+	String password;
+	Date dob;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+}
